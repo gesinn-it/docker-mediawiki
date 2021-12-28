4e7e06efe05b34f5bd595f5a806b773040cfda58
Alexander <gesinn-it-gea@gesinn.it>
2021-12-28 17:45:56 +0100

    Variants per database type (#1)
    * prepare to support variants per database type
    * upload database dumps as release assets
    * added support for MySQL

--- update.sh
+++ update.sh
@@ -3,37 +3,40 @@ set -euo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-mediawikiReleases=( "$@" )
+mediawikiReleases=("$@")
 if [ ${#mediawikiReleases[@]} -eq 0 ]; then
-	mediawikiReleases=( 1.*/ )
+  mediawikiReleases=(1.*/)
 fi
-mediawikiReleases=( "${mediawikiReleases[@]%/}" )
+mediawikiReleases=("${mediawikiReleases[@]%/}")
 
 echo ${mediawikiReleases[*]}
 
 declare -A composerVersion=(
-	[1.31]='1.10.24'
-	[default]='2.1.14'
+  [1.31]='1.10.24'
+  [default]='2.1.14'
 )
 
 function mediawiki_version() {
-	git ls-remote --sort=version:refname --tags https://github.com/wikimedia/mediawiki.git \
-		| cut -d/ -f3 \
-		| tr -d '^{}' \
-		| grep -E "^$1" \
-		| tail -1
+  git ls-remote --sort=version:refname --tags https://github.com/wikimedia/mediawiki.git |
+    cut -d/ -f3 |
+    tr -d '^{}' |
+    grep -E "^$1" |
+    tail -1
 }
 
 for mediawikiRelease in "${mediawikiReleases[@]}"; do
-	mediawikiReleaseDir="$mediawikiRelease"
-	mediawikiVersion="$(mediawiki_version $mediawikiRelease)"
-	composerVersion=${composerVersion[$mediawikiRelease]-${composerVersion[default]}}
+  mediawikiReleaseDir="$mediawikiRelease"
+  mediawikiVersion="$(mediawiki_version $mediawikiRelease)"
+  composerVersion=${composerVersion[$mediawikiRelease]-${composerVersion[default]}}
 
-	mkdir -p "$mediawikiReleaseDir"
+  for dbType in sqlite mysql; do
+    dir="$mediawikiReleaseDir/$dbType"
+    mkdir -p "$dir"
 
-	sed -r \
-		-e 's!%%MEDIAWIKI_VERSION%%!'"$mediawikiVersion"'!g' \
-		-e 's!%%MEDIAWIKI_MAJOR_VERSION%%!'"$mediawikiRelease"'!g' \
-		-e 's!%%COMPOSER_VERSION%%!'"$composerVersion"'!g' \
-		"Dockerfile.template" > "$mediawikiReleaseDir/Dockerfile"
+    sed -r \
+      -e 's!%%MEDIAWIKI_VERSION%%!'"$mediawikiVersion"'!g' \
+      -e 's!%%MEDIAWIKI_MAJOR_VERSION%%!'"$mediawikiRelease"'!g' \
+      -e 's!%%COMPOSER_VERSION%%!'"$composerVersion"'!g' \
+      "Dockerfile-${dbType}.template" >"$dir/Dockerfile"
+  done
 done
